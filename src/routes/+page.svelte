ccb266161d5a8a42cf9fa40cd6da017d7e00a2e2
Jonathan <jhang01@outlook.com>
2023-08-17 12:24:28 -0500

    Clean up code.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,16 +3,11 @@
 	import ContactSection from '$lib/sections/ContactSection.svelte';
 	import Projects from '$lib/sections/ProjectSection.svelte';
 	import WelcomeSection from '$lib/sections/HomeSection.svelte';
-	import BackToTop from '$lib/components/BackToTop.svelte';
-	// import HashChange from '$lib/components/HashChange.svelte';
 
 	export const prerender = true;
 </script>
 
-<!-- <HashChange /> -->
 <WelcomeSection />
-<!-- <slot/> -->
 <AboutSection />
 <Projects />
-<!-- <Timeline/> -->
 <ContactSection />
