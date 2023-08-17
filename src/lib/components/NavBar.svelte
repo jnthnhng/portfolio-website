8d9ee607202418eb48f02afc3968e17ec117a009
Jonathan <jhang01@outlook.com>
2023-08-16 22:18:07 -0500

    Implement sticky navbar to stay on top of the window through the site. git status

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -1,9 +1,10 @@
-<nav
-	class=" py-4 font-serif text-xl font-semibold leading-3 tracking-wide focus:underline-offset-0"
->
-	<ul class="flex justify-center space-x-4 text-gray-800">
+<!-- <nav class="py-4 font-serif text-xl font-semibold leading-3 tracking-wide focus:underline-offset-0"> -->
+<div class="bg-greenCream-cream fixed top-0 w-full shadow-md z-10 bg-opacity-50">
+	<nav class="container mx-auto px-6 py-3 font-serif text-xl font-semibold">
+	<ul class="flex justify-center space-x-4">
 		<li><a href="#about">About</a></li>
 		<li><a href="#projects">Projects</a></li>
 		<li><a href="#contact">Contact</a></li>
 	</ul>
 </nav>
+</div>
