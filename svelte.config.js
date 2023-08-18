cd3eca116167917edd5efd32ec0d039049a9da02
Jonathan <jhang01@outlook.com>
2023-08-18 18:59:21 -0500

    Reverted.

--- svelte.config.js
+++ svelte.config.js
@@ -1,3 +1,4 @@
+// import adapterCloudflare from '@sveltejs/adapter-cloudflare';
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
@@ -11,7 +12,33 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			// See below for an explanation of these options
+			routes: {
+				include: ['/*'],
+				exclude: ['<all>']
+			}
+		}),
+		alias: {
+			$components: './src/lib/components',
+			$static: './static'
+		},
+		csp: {
+			mode: 'auto',
+
+			directives: {
+				'script-src': ['self']
+			}
+			// reportOnly: {
+			// 	'script-src': ['self']
+			// }
+		},
+		csrf: {
+			checkOrigin: true
+		},
+		prerender: {
+			handleMissingId: 'warn'
+		}
 	}
 };
 
